0e678bf7c8dd35ea65244375750985f21d1f6919
Maciej Buca <maciekb94@o2.pl>
2016-06-16 15:09:21 +0200

    karcisz - receiver controller

---
+++ src/AppBundle/Controller/ReceiverController.php
@@ -0,0 +1,22 @@
+<?php
+namespace AppBundle\Controller;
+use FOS\RestBundle\Controller\FOSRestController;
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
+class ReceiverController extends FOSRestController {
+	
+	public function getReceiversAction()
+	{
+		$orders=$this->getDoctrine()->getRepository('AppBundle:ParcelOrder')->findAll();
+		foreach($orders as $order)
+		{
+			$receivers[]=$this->getDoctrine()->getRepository('AppBundle:AddressData')->find($order->getReceiver()->getId());
+		}
+		return $receivers;
+	}
+	
+	public function getReceiverAction($id)
+	{
+		$receiver=$this->getDoctrine()->getRepository('AppBundle:AddressData')->find($id);
+		return $receiver;
+	}
